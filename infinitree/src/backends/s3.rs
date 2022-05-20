4db29d578a367b0a756b341cc287dffc39b2e324
rsdy <p@symmetree.dev>
2022-05-20 20:53:22 +0100

    Rework S3 backend
    This patch introduces the backpressure, and migrates away from the
    `rust-s3` crate due to compatibility reasons.

--- infinitree/src/backends/s3.rs
+++ infinitree/src/backends/s3.rs
@@ -1,26 +1,136 @@
 use super::{Backend, Result};
 use crate::object::{Object, ObjectId, ReadBuffer, ReadObject, WriteObject};
-use ::s3::Bucket;
-pub use ::s3::{creds::Credentials, Region};
 use anyhow::Context;
+use reqwest::Client;
+pub use rusty_s3::{Bucket, Credentials};
+use rusty_s3::{S3Action, UrlStyle};
 use scc::HashMap;
-use std::sync::Arc;
+use std::{env, future::Future, sync::Arc, time::Duration};
 use tokio::{
     runtime,
-    task::{self, JoinHandle},
+    sync::Semaphore,
+    task::{self, JoinError, JoinHandle},
 };
 
-type TaskHandle = JoinHandle<anyhow::Result<u16>>;
+mod region;
+pub use region::Region;
+
+fn block_on<O>(fut: impl Future<Output = O>) -> O {
+    task::block_in_place(move || runtime::Handle::current().block_on(fut))
+}
+
+struct InFlightTracker<TaskResult>
+where
+    TaskResult: 'static + Send,
+{
+    permits: Arc<Semaphore>,
+    active: Arc<HashMap<ObjectId, Arc<Option<JoinHandle<TaskResult>>>>>,
+}
+
+impl<TaskResult> Default for InFlightTracker<TaskResult>
+where
+    TaskResult: 'static + Send,
+{
+    fn default() -> Self {
+        Self {
+            permits: Semaphore::new(std::thread::available_parallelism().unwrap().get()).into(),
+            active: Arc::default(),
+        }
+    }
+}
+
+impl<TaskResult> Clone for InFlightTracker<TaskResult>
+where
+    TaskResult: 'static + Send,
+{
+    fn clone(&self) -> Self {
+        Self {
+            permits: self.permits.clone(),
+            active: self.active.clone(),
+        }
+    }
+}
+
+impl<TaskResult> InFlightTracker<TaskResult>
+where
+    TaskResult: 'static + Send,
+{
+    pub fn complete_all(&self) -> std::result::Result<Vec<TaskResult>, JoinError> {
+        task::block_in_place(move || {
+            runtime::Handle::current().block_on(async move {
+                let mut handles = vec![];
+                self.active
+                    .retain_async(|_, v| {
+                        if let Some(handle) = std::mem::take(Arc::get_mut(v).unwrap()) {
+                            handles.push(handle);
+                        }
+
+                        false
+                    })
+                    .await;
+
+                futures::future::join_all(handles).await
+            })
+        })
+        .into_iter()
+        .filter(|result| match result {
+            Ok(_) => true,
+            Err(e) => !e.is_cancelled(),
+        })
+        .into_iter()
+        .collect::<std::result::Result<Vec<_>, _>>()
+    }
+
+    pub fn add_task<F: 'static + Send + Future<Output = TaskResult>>(
+        &self,
+        key: ObjectId,
+        task: F,
+    ) {
+        let permits = self.permits.clone();
+        let active = self.active.clone();
+
+        block_on(async {
+            let permit = permits.acquire_owned().await;
+
+            let handle = Arc::new(Some(task::spawn(async move {
+                let _permit = permit;
+                let result = task.await;
+                active.remove_async(&key).await;
+                result
+            })));
+
+            self.active
+                .upsert_async(
+                    key,
+                    || handle.clone(),
+                    |_, v| {
+                        if let Some(handle) = v.as_ref() {
+                            handle.abort();
+                        }
+
+                        *v = handle.clone();
+                    },
+                )
+                .await;
+        })
+    }
+}
 
 #[derive(Clone)]
 pub struct S3 {
-    client: Bucket,
-    in_flight: Arc<HashMap<ObjectId, Arc<Option<TaskHandle>>>>,
+    base_path: String,
+    client: Client,
+    bucket: Arc<Bucket>,
+    credentials: Arc<Credentials>,
+    in_flight: InFlightTracker<anyhow::Result<u16>>,
 }
 
 impl S3 {
     pub fn new(region: Region, bucket: impl AsRef<str>) -> Result<Arc<Self>> {
-        let creds = Credentials::default().context("Failed to get S3 credentials")?;
+        let access_key = env::var("AWS_ACCESS_KEY_ID").context("Invalid credentials")?;
+        let secret_key = env::var("AWS_SECRET_ACCESS_KEY").context("Invalid credentials")?;
+
+        let creds = Credentials::new(access_key, secret_key);
         Self::with_credentials(region, bucket, creds)
     }
 
@@ -29,63 +139,103 @@ impl S3 {
         bucket: impl AsRef<str>,
         creds: Credentials,
     ) -> Result<Arc<Self>> {
-        let client = Bucket::new(bucket.as_ref(), region, creds)
-            .context("Failed to connect to S3 bucket")?
-            .with_path_style();
+        let (bucket_name, base_path) = match bucket.as_ref().split_once('/') {
+            Some((bucket, "")) => (bucket.to_string(), "".to_string()),
+            Some((bucket, path)) => (bucket.to_string(), format!("{}/", path)),
+            None => (bucket.as_ref().to_string(), "".to_string()),
+        };
+
+        let bucket = Bucket::new(
+            region.endpoint().parse().context("Invalid endpoint URL")?,
+            if let Region::Custom { .. } = region {
+                UrlStyle::Path
+            } else {
+                UrlStyle::VirtualHost
+            },
+            bucket_name,
+            region.to_string(),
+        )
+        .context("Failed to connect to S3 bucket")?
+        .into();
 
         Ok(Self {
-            client,
-            in_flight: Arc::new(HashMap::default()),
+            bucket,
+            base_path,
+            client: reqwest::Client::new(),
+            credentials: creds.into(),
+            in_flight: InFlightTracker::default(),
         }
         .into())
+    }
+
+    fn get_path(&self, id: &ObjectId) -> String {
+        // note that `base_path` automatically has a "/" appended to the string
+        format!("{}{}", &self.base_path, id.to_string())
     }
 }
 
 impl Backend for S3 {
     fn write_object(&self, object: &WriteObject) -> Result<()> {
-        let client = self.client.clone();
-        let in_flight = self.in_flight.clone();
-
         let body = object.as_inner().to_vec();
-        let key = object.id().to_string();
+        let key = self.get_path(object.id());
         let id = *object.id();
 
-        let handle = Arc::new(Some(task::spawn(async move {
-            let status_code = client.put_object_stream(&mut body.as_slice(), key).await?;
-            in_flight.remove(&id);
+        let this = self.clone();
+        self.in_flight.add_task(id, async move {
+            let url = this
+                .bucket
+                .put_object(Some(&this.credentials), &key)
+                .sign(Duration::from_secs(30));
 
-            Ok(status_code)
-        })));
+            let resp = this
+                .client
+                .put(url)
+                .body(body)
+                .send()
+                .await
+                .expect("Server error");
 
-        self.in_flight.upsert(
-            id,
-            || handle.clone(),
-            |_, v| {
-                if let Some(handle) = v.as_ref() {
-                    handle.abort();
-                }
-
-                *v = handle.clone();
-            },
-        );
+            let status_code = resp.status().as_u16();
+            let resp_body = resp.bytes().await.expect("Response error");
+            if (200..300).contains(&status_code) {
+                Ok(status_code)
+            } else {
+                panic!(
+                    "Bad response: {}, {}",
+                    status_code,
+                    String::from_utf8_lossy(resp_body.as_ref())
+                )
+            }
+        });
 
         Ok(())
     }
 
     fn read_object(&self, id: &ObjectId) -> Result<Arc<ReadObject>> {
+        let this = self.clone();
         let object: Result<Vec<u8>> = {
-            let client = self.client.clone();
-            let key = id.to_string();
+            let key = self.get_path(id);
 
-            task::block_in_place(move || {
-                runtime::Handle::current().block_on(async move {
-                    let (buf, _status_code) = client
-                        .get_object(key)
-                        .await
-                        .context("Failed to fetch object")?;
+            block_on(async move {
+                let url = this
+                    .bucket
+                    .get_object(Some(&this.credentials), &key)
+                    .sign(Duration::from_secs(30));
 
-                    Ok(buf)
-                })
+                let resp = this.client.get(url).send().await.context("Query error")?;
+                let status_code = resp.status().as_u16();
+                let body = resp.bytes().await.context("Read error")?;
+
+                if (200..300).contains(&status_code) {
+                    Ok(body.to_vec())
+                } else {
+                    Err(anyhow::anyhow!(
+                        "Bad response: {}, {}",
+                        status_code,
+                        String::from_utf8_lossy(body.as_ref())
+                    )
+                    .into())
+                }
             })
         };
 
@@ -93,22 +243,9 @@ impl Backend for S3 {
     }
 
     fn sync(&self) -> Result<()> {
-        let handles = futures::stream::FuturesUnordered::new();
-        self.in_flight.for_each(|_, v| {
-            if let Some(handle) = std::mem::take(Arc::get_mut(v).unwrap()) {
-                handles.push(handle);
-            }
-        });
-
-        task::block_in_place(move || {
-            runtime::Handle::current()
-                .block_on(async move { futures::future::join_all(handles.into_iter()).await })
-        })
-        .into_iter()
-        .collect::<std::result::Result<Vec<_>, _>>()
-        .context("Failed to upload objects")?
-        .into_iter()
-        .collect::<std::result::Result<Vec<_>, _>>()?;
+        self.in_flight
+            .complete_all()
+            .context("Failed transactions with server")?;
 
         Ok(())
     }
@@ -130,10 +267,11 @@ mod test {
     };
     use tokio::task;
 
-    const AWS_ACCESS_KEY_ID: &'static str = "ANTN35UAENTS5UIAEATD";
-    const AWS_SECRET_ACCESS_KEY_ID: &'static str = "TtnuieannGt2rGuie2t8Tt7urarg5nauedRndrur";
+    const AWS_ACCESS_KEY_ID: &'static str = "MEEMIEW3EEKI8IEY1U";
+    const AWS_SECRET_ACCESS_KEY_ID: &'static str = "noh8xah2thohv7laehei2lahBuno5FameiNi";
 
-    const BIND_SERVER: ([u8; 4], u16) = ([127, 0, 0, 1], 12312);
+    const SERVER_ADDR_RW: ([u8; 4], u16) = ([127, 0, 0, 1], 12312);
+    const SERVER_ADDR_RO: ([u8; 4], u16) = ([127, 0, 0, 1], 12313);
 
     fn setup_s3_server(addr: &SocketAddr) {
         let fs = FileSystem::new(TEST_DATA_DIR).unwrap();
@@ -158,7 +296,7 @@ mod test {
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
     async fn s3_write_read() {
-        let addr = SocketAddr::from(BIND_SERVER);
+        let addr = SocketAddr::from(SERVER_ADDR_RW);
         setup_s3_server(&addr);
 
         let backend = S3::new(
@@ -180,7 +318,7 @@ mod test {
     #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
     #[should_panic]
     async fn s3_reading_nonexistent_object() {
-        let addr = SocketAddr::from(BIND_SERVER);
+        let addr = SocketAddr::from(SERVER_ADDR_RO);
         setup_s3_server(&addr);
 
         let backend = S3::new(
